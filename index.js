522ba037b6a354dc0f342bd7915ba0f864e2a513
Ben Helms <benhelms@pop-os.localdomain>
2021-06-04 11:37:25 -0500

    Finishing up

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || "8000";
+
+// TODO: With more time, extract to another file and improve guard clause.
+function fib(n) {
+  if(isNaN(n)){
+    return "Bad Value"
+  }
+  let [a, b] = [0, 1];
+  while (n-- > 0) {
+    [a, b] = [b, a + b];
+  }
+  return b;
+}
+
+app.get("/", (req, res) => {
+  res.status(200).send("Provide Route parameter to receive the nth value in a Fibonacci sequence. ie: {URL}/5");
+});
+
+
+// TODO: Add better error handling
+app.get("/:id", (req, res) => {
+  const fibNum = req.params.id
+  res.status(200).send(JSON.stringify({ NthValue: fib(fibNum) }));
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
